7872ea6389021e3540a7fe6ea7968d758179a3f1
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-24 15:39:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,17 @@ def line(katz_deli)
     puts "The line is currently empty." #ok
   else 
 
+  
+    newArray = []
     counter = 0   
     katz_deli.each do |x|
     counter += 1
-    puts  "The line is currently: " + "#{counter}. #{x}"
     
+    newArray << "#{counter}. #{x}"
+    #puts newArray
+    #outArray = newArray.to_s
+    end
+   puts  "The line is currently: " + newArray.to_s
     
     end
 
